639e1b654e0d90170e0c3431cd336bb647ce496d
“Malik <awan.jahangir.malik@gmail.com>
2022-07-10 07:03:24 +0500

    initial commit

--- menu.js
+++ menu.js
@@ -2,12 +2,13 @@
 const navBtn = document.querySelector(".hamburger-menu");
 const navMob = document.querySelector(".nav-mobile");
 const navOpenClose = function () {
+  // navMob.style.display='flex';
+
   navBtn.classList.toggle("hamburger-menu-active");
   navMob.classList.toggle("nav-active");
 };
 
 
-navMob.style.display='flex';
 navBtn.addEventListener("click", () => {
   
 navOpenClose();
